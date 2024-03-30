5f359f6f48166bcd7cb1f14c2d36a257ba5af5f9
Mr Alexandre J-S William ELISÉ <code@apiadept.com>
2024-03-30 14:58:36 -0400

    [chore] Optimize install-japp.sh and README.md file
    - Remove duplicate content already in .env file
    - Improve readability of README.md and ease of use

--- install-japp.sh
+++ install-japp.sh
@@ -3,9 +3,6 @@
 set -eu pipefail
 
 echo "Deployed auto-installed version of Joomla on FrankenPHP successfully" &&
-echo "Here are your Joomla credentials: username: ${JOOMLA_ADMIN_USERNAME}, password: ${JOOMLA_ADMIN_PASSWORD}" &&
-echo "Here are you MySQL credentials: username: root, password: ${MYSQL_ROOT_PASSWORD}" &&
-echo "PLEASE CHANGE IT ON FIRST LOGIN FOR MORE SECURITY. ANYONE, OR ANYTHING THAT HAS ACCESS TO THOSE LOGS CAN SEE YOUR CREDENTIALS!!!" &&
 frankenphp fmt --overwrite /etc/caddy/Caddyfile &&
 frankenphp validate --config /etc/caddy/Caddyfile &&
 frankenphp run --config /etc/caddy/Caddyfile
